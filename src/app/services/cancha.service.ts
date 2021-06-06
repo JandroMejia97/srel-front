88359434a00b3a6e4a55817ecda59dc732b381b4
Carlos Alejandro Hernández Mejía <alejandromejia2013.27@gmail.com>
2021-06-06 00:05:26 -0300

    Modificando la creación de la ruta para obtener las canchas

--- src/app/services/cancha.service.ts
+++ src/app/services/cancha.service.ts
@@ -1,95 +1,93 @@
-import { Injectable } from '@angular/core';
-import { HttpHeaders, HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
-import { tap, catchError } from 'rxjs/operators';
-import { Cancha } from '../models/cancha';
-import { MensajeService } from './mensaje.service';
-import { StorageService } from './storage.service';
-import { environment } from 'src/environments/environment';
+import { Injectable } from "@angular/core";
+import { HttpHeaders, HttpClient } from "@angular/common/http";
+import { Observable, of } from "rxjs";
+import { tap, catchError } from "rxjs/operators";
+import { Cancha } from "../models/cancha";
+import { MensajeService } from "./mensaje.service";
+import { StorageService } from "./storage.service";
+import { environment } from "src/environments/environment";
+import { Router } from "@angular/router";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class CanchaService {
   private canchasUrl = environment.apiUrl;
   private httpOptions = {
     headers: new HttpHeaders({
-      'Content-type': 'application/json',
-      Authorization: `token ${this.storageService.getCurrentToken()}`
-    })
+      "Content-type": "application/json",
+      Authorization: `token ${this.storageService.getCurrentToken()}`,
+    }),
   };
 
   constructor(
+    private router: Router,
     private http: HttpClient,
     private mensajeService: MensajeService,
     private storageService: StorageService
-  ) { }
+  ) {}
 
   getCanchas(...criterios: any[]): Observable<Cancha[]> {
-    let filterParameters = '';
-    if (criterios) {
-      filterParameters = '?';
-      criterios.forEach(criterio => {
-        for (const key in criterio) {
-          if (criterio.hasOwnProperty(key) && !isNaN(criterio[key])) {
-            filterParameters += `${key}=${criterio[key]}&`;
-          }
-        }
-      });
-    }
-    return this.http.get<Cancha[]>(
-      `${this.canchasUrl}/canchas${filterParameters}`,
-      this.httpOptions
-    ).pipe(
-      tap(_ => console.log('Datos recuperados exitosamente')),
-      catchError(this.handleError('getCanchas()', []))
-    );
+    const path = this.router.createUrlTree(["/canchas"], {
+      queryParams: { ...criterios[0] },
+    });
+    return this.http
+      .get<Cancha[]>(`${this.canchasUrl}${path.toString()}`, this.httpOptions)
+      .pipe(
+        tap((_) => console.log("Datos recuperados exitosamente")),
+        catchError(this.handleError("getCanchas()", []))
+      );
   }
 
   getCancha(id: number): Observable<Cancha> {
     const url = `${this.canchasUrl}/canchas/${id}`;
     return this.http.get<Cancha>(url, this.httpOptions).pipe(
-        tap(_ => console.log('Datos recuperados exitosamente')),
-        catchError(this.handleError<Cancha>(`getCancha(id=${id})`))
-      );
+      tap((_) => console.log("Datos recuperados exitosamente")),
+      catchError(this.handleError<Cancha>(`getCancha(id=${id})`))
+    );
   }
 
   updateCancha(cancha: Cancha): Observable<any> {
-    return this.http.put(`${this.canchasUrl}/canchas/${cancha.id}/`, cancha, this.httpOptions)
-    .pipe(
-      tap(_ => this.log('Datos actualizados exitosamente')),
-      catchError(this.handleError<any>(`updateCancha(id=${cancha.id}, name: ${cancha.nombre})`))
-    );
+    return this.http
+      .put(`${this.canchasUrl}/canchas/${cancha.id}/`, cancha, this.httpOptions)
+      .pipe(
+        tap((_) => this.log("Datos actualizados exitosamente")),
+        catchError(
+          this.handleError<any>(
+            `updateCancha(id=${cancha.id}, name: ${cancha.nombre})`
+          )
+        )
+      );
   }
 
   addCancha(cancha: Cancha): Observable<any> {
-    return this.http.post(`${this.canchasUrl}/canchas/`, cancha, this.httpOptions)
-    .pipe(
-      tap(_ => this.log('Cancha guardada exitosamente')),
-      catchError(this.handleError<Cancha>('addCancha()'))
-    );
+    return this.http
+      .post(`${this.canchasUrl}/canchas/`, cancha, this.httpOptions)
+      .pipe(
+        tap((_) => this.log("Cancha guardada exitosamente")),
+        catchError(this.handleError<Cancha>("addCancha()"))
+      );
   }
 
   deleteCancha(cancha: Cancha | number): Observable<Cancha> {
-    const id = typeof cancha === 'number' ? cancha : cancha.id;
+    const id = typeof cancha === "number" ? cancha : cancha.id;
     const url = `${this.canchasUrl}/canchas/${id}/`;
-    return this.http.delete<Cancha>(url, this.httpOptions)
-    .pipe(
-      tap(_ => this.log('Cancha eliminada exitosamente')),
+    return this.http.delete<Cancha>(url, this.httpOptions).pipe(
+      tap((_) => this.log("Cancha eliminada exitosamente")),
       catchError(this.handleError<Cancha>(`deleteCancha(id=${id})`))
     );
   }
 
   private log(mensaje: string) {
-    this.mensajeService.openSnackBar(`${ mensaje }`);
+    this.mensajeService.openSnackBar(`${mensaje}`);
   }
 
-  handleError<T>(operacion: string = 'operacion()', resultado?: T) {
+  handleError<T>(operacion: string = "operacion()", resultado?: T) {
     return (error: any): Observable<T> => {
       if (error.error) {
         for (const key in error.error) {
           if (error.error.hasOwnProperty(key)) {
-            this.log(`${(key).toUpperCase()}: ${error.error[key]}`);
+            this.log(`${key.toUpperCase()}: ${error.error[key]}`);
           }
         }
       }
